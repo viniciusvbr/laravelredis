b29b56c93f9606db7447d7055dd78c74246f4ec7
Vinicius Rodrigues <vrodrigues@salespond.com>
2020-11-23 15:42:46 +1100

    first commit

---
+++ src/app/Http/Controllers/UserController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Redis;
+
+class UserController extends Controller
+{
+    /**
+     * Show the profile for the given user.
+     *
+     * @param  int  $id
+     * @return Response
+     */
+    public function showProfile($id)
+    {
+        $user = Redis::get('user:profile:'.$id);
+
+        return view('user.profile', ['user' => $user]);
+    }
+}
